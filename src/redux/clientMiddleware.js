d383557742de8f06b054234444f1eca84f3e9813
Андрей Овчаренко <ovcharenkoav@meta.ua>
2017-07-17 14:10:51 +0300

    code clean

--- src/redux/clientMiddleware.js
+++ src/redux/clientMiddleware.js
@@ -1,29 +1,19 @@
-const clientMiddleware = axios =>
-  ({ dispatch, getState }) =>
-    next => (action) => {
-      if (typeof action === 'function') {
-        return action(dispatch, getState);
-      }
-
-      const { promise, events, ...rest } = action; // eslint-disable-line no-redeclare
-      if (!promise) {
-        return next(action);
-      }
-
-      const [REQUEST, SUCCESS, FAILURE] = events;
-      next({ ...rest, type: REQUEST });
-
-      const actionPromise = promise(axios);
-      console.log('in promise');
-      actionPromise.then(
-        res => console.log(res.data) || next({ ...rest, data: res.data, type: SUCCESS }),
-        err => console.log(err) || next({ ...rest, err, type: FAILURE }),
-      ).catch((err) => {
-        console.error('MIDDLEWARE ERROR:', err);
-        next({ ...rest, err, type: FAILURE });
-      });
-
-      return actionPromise;
-    };
-
-export default clientMiddleware;
+export default fetch => ({ dispatch, getState }) => next => (action) => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState);
+  }
+  const { promise, events, ...rest } = action;
+  if (!promise) {
+    return next(action);
+  }
+  const [REQUEST, SUCCESS, FAILURE] = events;
+  next({ ...rest, type: REQUEST });
+  const actionPromise = promise(fetch);
+  actionPromise.then(
+    res => next({ ...rest, data: res.data, type: SUCCESS }),
+    err => next({ ...rest, err, type: FAILURE }),
+  ).catch((err) => {
+    next({ ...rest, err, type: FAILURE });
+  });
+  return actionPromise;
+};
